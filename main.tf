7dc06715206e181f2703b1b73116db60ad5a9a64
axims1 <asim.ieee1@gmail.com>
2024-12-19 23:41:38 +1100

    SG CHANGE

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "aws_security_group" "blog" {
 }
 
 
-resource "aws_security_group_rule" "blog_http_in"{
+resource "aws_security_group_rule" "blog_http_in" {
   type        = "ingress"
   from_port   = 80
   to_port     = 80 
@@ -46,7 +46,7 @@ resource "aws_security_group_rule" "blog_http_in"{
   aws_security_group_id = aws_security_group.blog.id
 }
 
-resource "aws_security_group_rule" "blog_https_in"{
+resource "aws_security_group_rule" "blog_https_in" {
   type        = "ingress"
   from_port   = 443
   to_port     = 443
@@ -57,7 +57,7 @@ resource "aws_security_group_rule" "blog_https_in"{
 
 }
 
-resource "aws_security_group_rule" "blog_outbound"{
+resource "aws_security_group_rule" "blog_outbound" {
   type        = "egress"
   from_port   = 0 
   to_port     = 0
